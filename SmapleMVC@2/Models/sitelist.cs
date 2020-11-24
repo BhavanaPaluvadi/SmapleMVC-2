0972c98a308a9d4af7f1f570617bd5595d262f68
bhavanasupraja <bhavanasupraja@gmail.com>
2020-11-25 00:23:49 +0530

    addanewclass

---
+++ SmapleMVC@2/Models/sitelist.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmapleMVC_2.Models
+{
+    public class sitelist
+    {
+        public int MyProperty { get; set; }
+        public int MyProperty1 { get; set; }
+        public int MyProperty2 { get; set; }
+    }
+}
