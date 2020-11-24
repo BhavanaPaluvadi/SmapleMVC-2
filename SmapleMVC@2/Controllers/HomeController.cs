c216daff075ba55e03c635310a96a5f69773e9a4
bhavanasupraja <bhavanasupraja@gmail.com>
2020-11-25 00:21:00 +0530

    renamemethodname

--- SmapleMVC@2/Controllers/HomeController.cs
+++ SmapleMVC@2/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace SmapleMVC_2.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index1()
         {
             return View();
         }
